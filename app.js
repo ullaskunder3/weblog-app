7ecd4cc96a569a3af7f11d2c5dddcf6e078b536d
ullas <ullaskunder3@gmail.com>
2022-02-28 01:58:57 +0530

    added ejs routes, define static

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 
 app.use(express.static('public'));
+app.use('/public/img',express.static('./public/img'));
+app.use('/public/svgs',express.static('./public/svgs'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'index.html'))
+    res.render('index');
 })
 app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './public/about.html'))
+    res.render('about');
 })
-app.get('/contact', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './public/contact.html'))
+app.get('/login', (req, res)=>{
+    res.render('login')
 })
 app.get('/post', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './public/post.html'))
+    res.render('post')
 })
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
