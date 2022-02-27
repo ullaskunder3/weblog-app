730f3eaceba1c2e0cbc6d92f9e7fe0d7324bb253
ullas <ullaskunder3@gmail.com>
2022-02-28 02:18:28 +0530

    PORT, deploy error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const ejs = require('ejs');
 
 const app = express();
-const PORT = 3000||process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use('/public/img',express.static('./public/img'));
